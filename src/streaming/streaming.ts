2ae2a1d3a084d1e3c771d8b1a93721d4b560bd5c
Pierre Bresson <PierreBresson@users.noreply.github.com>
2020-05-01 18:24:14 +0200

    improve data

--- src/streaming/streaming.ts
+++ src/streaming/streaming.ts
@@ -1,10 +1,16 @@
 /* Unit: bits/s, use getInternetUsageCarbonImpact to get kgCO₂eq */
 
-const HDVideo = (700 * Math.pow(10, 6) * 8) / (120 * 60);
-const fullHDVideo = (3400 * Math.pow(10, 6) * 8) / (120 * 60);
-const ultraHDVideo = (14300 * Math.pow(10, 6) * 8) / (120 * 60);
+/* 
+  Star Wars The Rise Of Skywalker - 2h22
+  HD / 720p : 1.21 GB
+  Full HD / 1080p : 7.02 GB
+  Ultra HD / 2160p : 35.73 Gb
+ */
+const HDVideo = (1.21 * Math.pow(10, 9) * 8) / (2 * 60 + 22);
+const fullHDVideo = (7.02 * Math.pow(10, 9) * 8) / (2 * 60 + 22);
+const ultraHDVideo = (35.73 * Math.pow(10, 9) * 8) / (2 * 60 + 22);
 
-/* 312 kbps */
+/* MP3 song at 312 kbps / 6.6 MB */
 const audioMP3 = (6.6 * Math.pow(10, 6) * 8) / 210;
 
 export default {

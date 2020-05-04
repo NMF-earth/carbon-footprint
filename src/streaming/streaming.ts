df477fd8fd0cf5248c2be2246be0f13c4b62b00d
Pierre Bresson <PierreBresson@users.noreply.github.com>
2020-05-04 18:18:50 +0200

    fix streaming const

--- src/streaming/streaming.ts
+++ src/streaming/streaming.ts
@@ -6,9 +6,9 @@
   Full HD / 1080p : 7.02 GB
   Ultra HD / 2160p : 35.73 Gb
  */
-const HDVideo = (1.21 * Math.pow(10, 9) * 8) / (2 * 60 + 22);
-const fullHDVideo = (7.02 * Math.pow(10, 9) * 8) / (2 * 60 + 22);
-const ultraHDVideo = (35.73 * Math.pow(10, 9) * 8) / (2 * 60 + 22);
+const HDVideo = (1.21 * Math.pow(10, 9) * 8) / ((2 * 60 + 22) * 60);
+const fullHDVideo = (7.02 * Math.pow(10, 9) * 8) / ((2 * 60 + 22) * 60);
+const ultraHDVideo = (35.73 * Math.pow(10, 9) * 8) / ((2 * 60 + 22) * 60);
 
 /* MP3 song at 312 kbps / 6.6 MB */
 const audioMP3 = (6.6 * Math.pow(10, 6) * 8) / 210;

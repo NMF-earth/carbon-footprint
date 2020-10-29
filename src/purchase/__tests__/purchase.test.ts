e38b73e516ae01bb3990e53c58a951354fd02cbc
Norman Steger <me@normansteger.com>
2020-10-29 20:21:14 +0100

    add test to check purchase enums count

--- src/purchase/__tests__/purchase.test.ts
+++ src/purchase/__tests__/purchase.test.ts
@@ -1,7 +1,15 @@
 import { keys, map } from 'ramda';
 import purchase from '../purchase';
+import { PurchaseType } from '../purchase.enum';
 
 test('All purchase keys should contain a valid number', () => {
   const purchaseKeys = keys(purchase);
   map((item: number) => expect(typeof purchaseKeys[item]).toBe('number'));
+});
+
+test('Number of default exports equals number of enums', () => {
+  const enumsCount = Object.keys(PurchaseType).length;
+  const defaultExportsCount = Object.keys(purchase).length;
+
+  expect(defaultExportsCount).toEqual(enumsCount);
 });
